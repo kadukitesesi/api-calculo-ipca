3882cea06db9509f80e7d17a806660a1d34fdb20
Mestre <kadukitesesi@gmail.com>
2023-12-31 08:19:38 -0300

    criação do calculo de ipca

--- app.js
+++ app.js
@@ -11,6 +11,36 @@ const filtra = (ano) => {
     }
 }
 
+
+const calcularReajuste = (valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno) => {
+    const historicoFiltrado = historicoInflacao.filter(
+      historico => {
+        if (dataInicialAno === dataFinalAno) {
+          return historico.ano === dataInicialAno && historico.mes >= dataInicialMes && historico.mes <= dataFinalMes;
+        } else {
+          return (
+            (historico.ano === dataInicialAno && historico.mes >= dataInicialMes) ||
+            (historico.ano > dataInicialAno && historico.ano < dataFinalAno) ||
+            (historico.ano === dataFinalAno && historico.mes <= dataFinalMes)
+          );
+        }
+      }
+    )};
+
+    app.get('/historicoIPCA/calculo', (req, res) => {
+        const valor = parseFloat(req.query.valor);
+        const dataInicialMes = parseInt(req.query.mesInicial);
+        const dataInicialAno = parseInt(req.query.anoInicial);
+        const dataFinalMes = parseInt(req.query.mesFinal);
+        const dataFinalAno = parseInt(req.query.anoFinal);
+      
+        if (validacaoErro(valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno)) {
+          res.status(400).json({ erro: 'Parâmetros inválidos' });
+          return;
+        }
+    })
+
+
 app.get('/historicoIPCA', (req, res) => {
     const ipcaAno = parseInt(req.query.ano); 
     const resultado = filtra(ipcaAno);
