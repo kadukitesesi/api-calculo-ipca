952fae711d3091f835dc65944177e9d198168c8d
Mestre <kadukitesesi@gmail.com>
2024-01-11 19:15:22 -0300

    calculo

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ const filtra = (ano) => {
 }
 
 
+export const validacaoErro = (valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno) => {
+    const anoLimiteFinal = historicoInflacao[historicoInflacao.length - 1].ano;
+    const anoLimiteInicial = historicoInflacao[0].ano
+    const mesLimiteFinal = historicoInflacao[historicoInflacao.length - 1].mes;
+    if (
+      isNaN(valor) ||
+      isNaN(dataInicialMes) ||
+      isNaN(dataInicialAno) ||
+      isNaN(dataFinalMes) ||
+      isNaN(dataFinalAno) ||
+      dataInicialMes < 1 || dataInicialMes > 12 ||
+      dataInicialAno < anoLimiteInicial || dataInicialAno > anoLimiteFinal ||
+      dataFinalMes < 1 || dataFinalMes > 12 ||
+      dataFinalAno < anoLimiteInicial || dataFinalAno > anoLimiteFinal ||
+      (dataFinalAno === anoLimiteFinal && dataFinalMes > mesLimiteFinal) ||
+      dataFinalAno < dataInicialAno ||
+      (dataFinalAno == dataInicialAno && dataFinalMes < dataInicialMes)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+
 const calcularReajuste = (valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno) => {
     const historicoFiltrado = historicoInflacao.filter(
       historico => {
